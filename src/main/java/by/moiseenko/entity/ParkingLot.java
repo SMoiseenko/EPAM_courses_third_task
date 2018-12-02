93abb818a1b4e60af3fb0ef1c47485b0d7fc9133
s-moiseenko <s-moiseenko@MyLaptop>
2018-12-02 18:54:39 +0300

    Big refactoring.
    Moved lots of method to the thread class. Leave only getters and
    setters.

--- src/main/java/by/moiseenko/entity/ParkingLot.java
+++ src/main/java/by/moiseenko/entity/ParkingLot.java
@@ -1,30 +1,22 @@
 package by.moiseenko.entity;
 
-import org.apache.log4j.Logger;
-
 /**
- * Class that emulate parking lot occupied by car for 2000ms;
+ * Class that emulate single parking lot with their id;
+ * 
  * @author moiseenko-s
  *
  */
 public class ParkingLot {
-    private static final Logger logger = Logger.getLogger(ParkingLot.class);
+
     private int lotID;
 
     public ParkingLot(int lotID) {
+	super();
 	this.lotID = lotID;
     }
 
     public int getLotID() {
 	return lotID;
-    }
-
-    public void parkedAtLot() {
-	try {
-	    Thread.sleep(2000);
-	} catch (InterruptedException e) {
-	    logger.error("Thread was interrupted!!!");
-	}
     }
 
 }
